40d785fe5cbbc09fa60c5938b7bbfd0ca1e592d5
Beyza Nur Çetiner <190101036@ogrenci.yalova.edu.tr>
2021-08-26 22:55:00 +0300

    Add project files.

---
+++ KampOdev2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KampOdev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> isimler = new MyDictionary<int, string>();
+            isimler.Add(20, "Beyza");
+            //Console.WriteLine(isimler.Length);
+            isimler.Add(19, "Su");
+            //Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Values)
+            {
+                Console.WriteLine(isim);
+            }
+
+            Console.WriteLine("Length: {0} ", isimler.Length);
+
+
+
+            Console.ReadLine();
+        
+        
+        }
+    }
+}
